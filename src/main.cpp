ff4e89f263259cdc707c58631820ac3cd6379789
Christian Oliveros <christianol_01@hotmail.com>
2018-09-09 18:49:00 -0400

    Almost glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,35 @@
-// * Taken from http://blog.johannesmp.com/2015/09/01/installing-clang-on-windows-pt2/
-// * Test if stl is properly linked
-// * In case of linker warning use clang-cl for windows 
-//   https://stackoverflow.com/questions/42545078/clang-version-5-and-lnk4217-warning
-//   https://stackoverflow.com/questions/50274547/windows-clang-hello-world-lnk4217
-#include <iostream>
-#include <vector>
-
-// Test if glfw3 was included
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-int main()
-{
-  std::vector<int> vect {1, 2, 3, 4, 5};
-  for(auto& el : vect)
-    std::cout << " - " << el << std::endl;
-  
-  return 0;
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported" << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
