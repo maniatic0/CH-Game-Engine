883492b1d0e508860eff4aa65a72cc41a041c38e
Christian Oliveros <christianol_01@hotmail.com>
2018-09-10 16:02:20 -0400

    Fixed win batch file flow

--- win-clang-make.bat
+++ win-clang-make.bat
@@ -1,13 +1,13 @@
 @echo off
 IF %1.==. GOTO No1
 
+cmake -H. -DCMAKE_BUILD_TYPE=%1 -Bbuilds/ -T"LLVM-vs2017" -G "Visual Studio 15 2017 Win64"
+cmake --build builds\ --target install --config %1
+
 
 :No1
   ECHO Requires Param Release^|^Debug^|RelWithDebInfo^|etc... check CMAKE_CONFIGURATION_TYPES
 GOTO End
-
-cmake -H. -DCMAKE_BUILD_TYPE=%1 -Bbuilds/ -T"LLVM-vs2017" -G "Visual Studio 15 2017 Win64"
-cmake --build builds\ --target install --config %1
 
 :End
 @echo on
