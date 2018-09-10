9128b90b711d933feae778cd3b32222d3f373f51
Christian Oliveros <christianol_01@hotmail.com>
2018-09-10 18:54:24 -0400

    Added Ref for GLM Copy Step and Fixed Typo in Readme

--- win-clang-make.bat
+++ win-clang-make.bat
@@ -3,7 +3,7 @@ IF %1.==. GOTO No1
 
 cmake -H. -DCMAKE_BUILD_TYPE=%1 -Bbuilds/ -T"LLVM-vs2017" -G "Visual Studio 15 2017 Win64"
 cmake --build builds\ --target install --config %1
-
+GOTO End
 
 :No1
   ECHO Requires Param Release^|^Debug^|RelWithDebInfo^|etc... check CMAKE_CONFIGURATION_TYPES
