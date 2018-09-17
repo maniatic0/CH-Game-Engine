8e97e8e12c0958391cd5404363b9163cf024f31e
Christian Oliveros <christianol_01@hotmail.com>
2018-09-16 23:26:33 -0400

    Vulkan Image Views

--- src/main.cpp
+++ src/main.cpp
@@ -98,6 +98,8 @@ class HelloTriangleApplication {
   VkFormat swapChainImageFormat;
   VkExtent2D swapChainExtent;
 
+  std::vector<VkImageView> swapChainImageViews;
+
   void initWindow() {
     LOG("Window Init Started");
     glfwInit();
@@ -274,19 +276,6 @@ class HelloTriangleApplication {
     }
 
     LOG("Vulkan Instance Init Successful");
-  }
-
-  void initVulkan() {
-    LOG("Vulkan Init Started");
-
-    createInstance();
-    setupDebugCallback();
-    createSurface();
-    pickPhysicalDevice();
-    createLogicalDevice();
-    createSwapChain();
-
-    LOG("Vulkan Init Successful");
   }
 
   void createSurface() {
@@ -746,6 +735,62 @@ class HelloTriangleApplication {
     LOG("Vulkan SwapChain Creation Successful");
   }
 
+  void createImageViews() {
+    LOG("Vulkan Image View Creation Started");
+    swapChainImageViews.resize(swapChainImages.size());
+
+    for (size_t i = 0; i < swapChainImages.size(); i++) {
+      VkImageViewCreateInfo createInfo = {};
+      createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+      createInfo.image = swapChainImages[i];
+
+      // The viewType and format fields specify how the image data should be
+      // interpreted. The viewType parameter allows you to treat images as 1D
+      // textures, 2D textures, 3D textures and cube maps.
+      createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+      createInfo.format = swapChainImageFormat;
+
+      // The components field allows you to swizzle the color channels around.
+      // For example, you can map all of the channels to the red channel for a
+      // monochrome texture. You can also map constant values of 0 and 1 to a
+      // channel. In our case we'll stick to the default mapping.
+      createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+      createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+      // The subresourceRange field describes what the image's purpose is and
+      // which part of the image should be accessed. Our images will be used as
+      // color targets without any mipmapping levels or multiple layers.
+      createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+      createInfo.subresourceRange.baseMipLevel = 0;
+      createInfo.subresourceRange.levelCount = 1;
+      createInfo.subresourceRange.baseArrayLayer = 0;
+      createInfo.subresourceRange.layerCount = 1;
+
+      if (vkCreateImageView(device, &createInfo, nullptr,
+                            &swapChainImageViews[i]) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create image views!");
+      }
+    }
+
+    LOG("Vulkan Image View Creation Successful");
+  }
+
+  void initVulkan() {
+    LOG("Vulkan Init Started");
+
+    createInstance();
+    setupDebugCallback();
+    createSurface();
+    pickPhysicalDevice();
+    createLogicalDevice();
+    createSwapChain();
+    createImageViews();
+
+    LOG("Vulkan Init Successful");
+  }
+
   void mainLoop() {
     while (!glfwWindowShouldClose(window)) {
       glfwPollEvents();
@@ -754,6 +799,10 @@ class HelloTriangleApplication {
 
   void cleanup() {
     LOG("Cleanup Started");
+
+    for (auto imageView : swapChainImageViews) {
+      vkDestroyImageView(device, imageView, nullptr);
+    }
 
     vkDestroySwapchainKHR(device, swapChain, nullptr);
 
