3fb3608df5b54d5b30c990bfe27f51fd43b24480
Christian Oliveros <christianol_01@hotmail.com>
2018-09-16 22:04:43 -0400

    Corrected Build foder type when building with cmake

--- win-clang-make.bat
+++ win-clang-make.bat
@@ -2,7 +2,7 @@
 IF %1.==. GOTO No1
 
 cmake -H. -DCMAKE_BUILD_TYPE=%1 -Bbuild/ -T"LLVM-vs2017" -G "Visual Studio 15 2017 Win64"
-cmake --build builds\ --target install --config %1
+cmake --build build\ --target install --config %1
 GOTO End
 
 :No1
