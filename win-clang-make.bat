60e8711df34a88babd7591d4874f5d0096a6b1c6
Christian Oliveros <christianol_01@hotmail.com>
2018-12-23 22:01:12 -0400

    Added offline compilation

--- win-clang-make.bat
+++ win-clang-make.bat
@@ -1,8 +1,14 @@
 @echo off
 IF %1.==. GOTO No1
+IF %2.==. (
+  SET OFFLINE_BUILD=OFF
+) ELSE (
+  SET OFFLINE_BUILD=%2
+)
+echo Performing OFFLINE_BUILD=%OFFLINE_BUILD% Note: it should be ON or OFF
 
 cmake -H. -DCMAKE_BUILD_TYPE=%1 -Bbuild/ -T"llvm" -G "Visual Studio 15 2017 Win64"
-cmake --build build\ --target install --config %1
+cmake -DBUILD_OFFLINE=%OFFLINE_BUILD% --build build\ --target install --config %1
 GOTO End
 
 :No1
