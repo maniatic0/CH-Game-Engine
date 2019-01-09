6d2acac1d5a5e7df97fe8a693040841fd77d7676
Christian Oliveros <christianol_01@hotmail.com>
2019-01-09 15:02:41 -0400

    Vertex Shader and Pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+
 #include <algorithm>
+#include <array>
 #include <cstdlib>
 #include <cstring>
 #include <functional>
@@ -15,6 +18,56 @@
 #include <utils/debug/log.hpp>
 #include <utils/files/binary_loader.hpp>
 #include <utils/files/file_path.hpp>
+
+// TODO: Move Vertex Config to new file
+// Vertex Config
+struct Vertex {
+  glm::vec2 pos;
+  glm::vec3 color;
+
+  static VkVertexInputBindingDescription getBindingDescription() {
+    /*
+    All of our per-vertex data is packed together in one array, so we're only
+    going to have one binding. The binding parameter specifies the index of the
+    binding in the array of bindings. The stride parameter specifies the number
+    of bytes from one entry to the next, and the inputRate parameter can have
+    one of the following values:
+
+    VK_VERTEX_INPUT_RATE_VERTEX: Move to the next data entry after each vertex
+    VK_VERTEX_INPUT_RATE_INSTANCE: Move to the next data entry after each
+    instance We're not going to use instanced rendering, so we'll stick to
+    per-vertex data.
+    */
+
+    VkVertexInputBindingDescription bindingDescription = {};
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof(Vertex);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+    return bindingDescription;
+  }
+
+  static std::array<VkVertexInputAttributeDescription, 2>
+  getAttributeDescriptions() {
+    std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions = {};
+
+    attributeDescriptions[0].binding = 0;
+    attributeDescriptions[0].location = 0;
+    attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+    attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+    attributeDescriptions[1].binding = 0;
+    attributeDescriptions[1].location = 1;
+    attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+    attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+    return attributeDescriptions;
+  }
+};
+
+const std::vector<Vertex> vertices = {{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+                                      {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+                                      {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
 
 // GLFW Config
 const int WIDTH = 800;
@@ -973,10 +1026,21 @@ class HelloTriangleApplication {
     VkPipelineVertexInputStateCreateInfo vertexInputInfo = {};
     vertexInputInfo.sType =
         VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    auto bindingDescription = Vertex::getBindingDescription();
+    auto attributeDescriptions = Vertex::getAttributeDescriptions();
+
+    vertexInputInfo.vertexBindingDescriptionCount = 1;
+    vertexInputInfo.vertexAttributeDescriptionCount =
+        static_cast<uint32_t>(attributeDescriptions.size());
+    vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
+    vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
+    /*
+    For no vertex data
     vertexInputInfo.vertexBindingDescriptionCount = 0;
     vertexInputInfo.pVertexBindingDescriptions = nullptr;  // Optional
     vertexInputInfo.vertexAttributeDescriptionCount = 0;
     vertexInputInfo.pVertexAttributeDescriptions = nullptr;  // Optional
+    */
 
     // Vertex Data Description
     VkPipelineInputAssemblyStateCreateInfo inputAssembly = {};
