36e388a5e093800526496e421fbcb7e1af6e95a9
Christian Oliveros <christianol_01@hotmail.com>
2019-01-12 21:44:07 -0400

    Index Buffer

--- src/main.cpp
+++ src/main.cpp
@@ -65,9 +65,12 @@ struct Vertex {
   }
 };
 
-const std::vector<Vertex> vertices = {{{0.0f, -0.5f}, {1.0f, 1.0f, 1.0f}},
-                                      {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-                                      {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
+const std::vector<Vertex> vertices = {{{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+                                      {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
+                                      {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+                                      {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}};
+
+const std::vector<uint16_t> indices = {0, 1, 2, 2, 3, 0};
 
 // GLFW Config
 const int WIDTH = 800;
@@ -182,6 +185,10 @@ class HelloTriangleApplication {
   // Vertex Buffer
   VkBuffer vertexBuffer;
   VkDeviceMemory vertexBufferMemory;
+
+  // Index Buffer
+  VkBuffer indexBuffer;
+  VkDeviceMemory indexBufferMemory;
 
   void initWindow() {
     LOG("Window Init Started");
@@ -1461,8 +1468,10 @@ class HelloTriangleApplication {
       VkDeviceSize offsets[] = {0};
       vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
 
-      vkCmdDraw(commandBuffers[i], static_cast<uint32_t>(vertices.size()), 1, 0,
-                0);
+      vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0,
+                           VK_INDEX_TYPE_UINT16); // VK_INDEX_TYPE_UINT16 because we are using uint_16
+
+      vkCmdDrawIndexed(commandBuffers[i], static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
 
       vkCmdEndRenderPass(commandBuffers[i]);
 
@@ -1636,6 +1645,34 @@ class HelloTriangleApplication {
     LOG("Vulkan Vertex Buffer Creation Successful");
   }
 
+  void createIndexBuffer() {
+    LOG("Vulkan Index Buffer Creation Successful");
+    VkDeviceSize bufferSize = sizeof(indices[0]) * indices.size();
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                     VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                 stagingBuffer, stagingBufferMemory);
+
+    void *data;
+    vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, indices.data(), (size_t)bufferSize);
+    vkUnmapMemory(device, stagingBufferMemory);
+
+    createBuffer(
+        bufferSize,
+        VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, indexBuffer, indexBufferMemory);
+
+    copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingBufferMemory, nullptr);
+    LOG("Vulkan Index Buffer Creation Successful");
+  }
+
   void cleanupSwapChain() {
     LOG("Vulkan SwapChain Cleanup Started");
     for (size_t i = 0; i < swapChainFramebuffers.size(); i++) {
@@ -1699,6 +1736,7 @@ class HelloTriangleApplication {
     createFramebuffers();
     createCommandPool();
     createVertexBuffer();
+    createIndexBuffer();
     createCommandBuffers();
     createSyncObjects();
 
@@ -1790,6 +1828,9 @@ class HelloTriangleApplication {
     LOG("Cleanup Started");
 
     cleanupSwapChain();
+
+    vkDestroyBuffer(device, indexBuffer, nullptr);
+    vkFreeMemory(device, indexBufferMemory, nullptr);
 
     vkDestroyBuffer(device, vertexBuffer, nullptr);
     vkFreeMemory(device, vertexBufferMemory, nullptr);
