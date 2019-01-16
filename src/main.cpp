c77091890a05033ecf44a3b7accae6c7cca321a9
Christian Oliveros <christianol_01@hotmail.com>
2019-01-16 15:23:29 -0400

    Image Sampler and Anisotropic Sampler Support

--- src/main.cpp
+++ src/main.cpp
@@ -226,6 +226,8 @@ class HelloTriangleApplication {
   // Image
   VkImage textureImage;
   VkDeviceMemory textureImageMemory;
+  VkImageView textureImageView;
+  VkSampler textureSampler;
 
   void initWindow() {
     LOG("Window Init Started");
@@ -585,8 +587,19 @@ class HelloTriangleApplication {
     }
 
     // Discrete GPUs have a significant performance advantage
+    LOG("\t\tDiscrete GPU: "
+        << (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU
+                ? "Found"
+                : "Not Found"));
     if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
       score += 1000;
+    }
+
+    // Support for Anisotropy Samplers
+    LOG("\t\tSupport for Anisotropy Samplers: "
+        << (deviceFeatures.samplerAnisotropy ? "Found" : "Not Found"));
+    if (deviceFeatures.samplerAnisotropy) {
+      return 500;
     }
 
     // Maximum possible size of textures affects graphics quality
@@ -681,8 +694,15 @@ class HelloTriangleApplication {
       queueCreateInfos.push_back(queueCreateInfo);
     }
 
-    // Required Device Features (I.e geomtry shader)
+    // Required Device Features (I.e geometry shader)
     VkPhysicalDeviceFeatures deviceFeatures = {};
+    VkPhysicalDeviceFeatures supportedFeatures;
+    vkGetPhysicalDeviceFeatures(physicalDevice, &supportedFeatures);
+    if (supportedFeatures.samplerAnisotropy) {
+      deviceFeatures.samplerAnisotropy = VK_TRUE;
+    } else {
+      deviceFeatures.samplerAnisotropy = VK_FALSE;
+    }
 
     // Logical Device Create Info
     VkDeviceCreateInfo createInfo = {};
@@ -893,43 +913,52 @@ class HelloTriangleApplication {
     LOG("Vulkan SwapChain Creation Successful");
   }
 
+  VkImageView createImageView(VkImage image, VkFormat format) {
+    VkImageViewCreateInfo viewInfo = {};
+
+    viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+    viewInfo.image = image;
+
+    // The viewType and format fields specify how the image data should be
+    // interpreted. The viewType parameter allows you to treat images as 1D
+    // textures, 2D textures, 3D textures and cube maps.
+    viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+    viewInfo.format = format;
+
+    // The components field allows you to swizzle the color channels around.
+    // For example, you can map all of the channels to the red channel for a
+    // monochrome texture. You can also map constant values of 0 and 1 to a
+    // channel. In our case we'll stick to the default mapping.
+    viewInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+    viewInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+    viewInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+    viewInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+    // The subresourceRange field describes what the image's purpose is and
+    // which part of the image should be accessed. Our images will be used as
+    // color targets without any mipmapping levels or multiple layers.
+    viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    viewInfo.subresourceRange.baseMipLevel = 0;
+    viewInfo.subresourceRange.levelCount = 1;
+    viewInfo.subresourceRange.baseArrayLayer = 0;
+    viewInfo.subresourceRange.layerCount = 1;
+
+    VkImageView imageView;
+    if (vkCreateImageView(device, &viewInfo, nullptr, &imageView) !=
+        VK_SUCCESS) {
+      throw std::runtime_error("failed to create texture image view!");
+    }
+
+    return imageView;
+  }
+
   void createImageViews() {
     LOG("Vulkan Image View Creation Started");
     swapChainImageViews.resize(swapChainImages.size());
 
     for (size_t i = 0; i < swapChainImages.size(); i++) {
-      VkImageViewCreateInfo createInfo = {};
-      createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-      createInfo.image = swapChainImages[i];
-
-      // The viewType and format fields specify how the image data should be
-      // interpreted. The viewType parameter allows you to treat images as 1D
-      // textures, 2D textures, 3D textures and cube maps.
-      createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-      createInfo.format = swapChainImageFormat;
-
-      // The components field allows you to swizzle the color channels around.
-      // For example, you can map all of the channels to the red channel for a
-      // monochrome texture. You can also map constant values of 0 and 1 to a
-      // channel. In our case we'll stick to the default mapping.
-      createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
-      createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
-      createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
-      createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
-
-      // The subresourceRange field describes what the image's purpose is and
-      // which part of the image should be accessed. Our images will be used as
-      // color targets without any mipmapping levels or multiple layers.
-      createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-      createInfo.subresourceRange.baseMipLevel = 0;
-      createInfo.subresourceRange.levelCount = 1;
-      createInfo.subresourceRange.baseArrayLayer = 0;
-      createInfo.subresourceRange.layerCount = 1;
-
-      if (vkCreateImageView(device, &createInfo, nullptr,
-                            &swapChainImageViews[i]) != VK_SUCCESS) {
-        throw std::runtime_error("failed to create image views!");
-      }
+      swapChainImageViews[i] =
+          createImageView(swapChainImages[i], swapChainImageFormat);
     }
 
     LOG("Vulkan Image View Creation Successful");
@@ -1828,6 +1857,81 @@ class HelloTriangleApplication {
     LOG("Vulkan Texture Loading Successful");
   }
 
+  void createTextureImageView() {
+    LOG("Vulkan Texture Image View Creation Started");
+    textureImageView = createImageView(textureImage, VK_FORMAT_R8G8B8A8_UNORM);
+    LOG("Vulkan Texture Image View Creation Successful");
+  }
+
+  void createTextureSampler() {
+    LOG("Vulkan Texture Sampler Creation Started");
+    VkSamplerCreateInfo samplerInfo = {};
+    samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+    // The magFilter and minFilter fields specify how to interpolate texels that
+    // are magnified or minified. Magnification concerns the oversampling
+    // problem describes above, and minification concerns undersampling. The
+    // choices are VK_FILTER_NEAREST and VK_FILTER_LINEAR, corresponding to the
+    // modes demonstrated in the images above.
+    samplerInfo.magFilter = VK_FILTER_LINEAR;
+    samplerInfo.minFilter = VK_FILTER_LINEAR;
+
+    /* The addressing mode can be specified per axis using the addressMode
+    fields. The available values are listed below. Most of these are
+    demonstrated in the image above. Note that the axes are called U, V and W
+    instead of X, Y and Z. This is a convention for texture space coordinates.
+
+    VK_SAMPLER_ADDRESS_MODE_REPEAT: Repeat the texture when going beyond the
+    image dimensions. VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT: Like repeat, but
+    inverts the coordinates to mirror the image when going beyond the
+    dimensions. VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE: Take the color of the
+    edge closest to the coordinate beyond the image dimensions.
+    VK_SAMPLER_ADDRESS_MODE_MIRROR_CLAMP_TO_EDGE: Like clamp to edge, but
+    instead uses the edge opposite to the closest edge.
+    VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_BORDER: Return a solid color when sampling
+    beyond the dimensions of the image. */
+    samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+
+    VkPhysicalDeviceFeatures supportedFeatures;
+    vkGetPhysicalDeviceFeatures(physicalDevice, &supportedFeatures);
+    if (supportedFeatures.samplerAnisotropy) {
+      samplerInfo.anisotropyEnable = VK_TRUE;
+      samplerInfo.maxAnisotropy = 16;
+    } else {
+      samplerInfo.anisotropyEnable = VK_FALSE;
+      samplerInfo.maxAnisotropy = 1;
+    }
+
+    /* The unnormalizedCoordinates field specifies which coordinate system you
+     * want to use to address texels in an image. If this field is VK_TRUE, then
+     * you can simply use coordinates within the [0, texWidth) and [0,
+     * texHeight) range. If it is VK_FALSE, then the texels are addressed using
+     * the [0, 1) range on all axes. Real-world applications almost always use
+     * normalized coordinates, because then it's possible to use textures of
+     * varying resolutions with the exact same coordinates. */
+    samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+
+    /* If a comparison function is enabled, then texels will first be compared
+     * to a value, and the result of that comparison is used in filtering
+     * operations. This is mainly used for percentage-closer filtering on shadow
+     * maps. */
+    samplerInfo.compareEnable = VK_FALSE;
+    samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+
+    samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+    samplerInfo.mipLodBias = 0.0f;
+    samplerInfo.minLod = 0.0f;
+    samplerInfo.maxLod = 0.0f;
+
+    if (vkCreateSampler(device, &samplerInfo, nullptr, &textureSampler) !=
+        VK_SUCCESS) {
+      throw std::runtime_error("failed to create texture sampler!");
+    }
+
+    LOG("Vulkan Texture Sampler Creation Successful");
+  }
+
   void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage,
                     VkMemoryPropertyFlags properties, VkBuffer &buffer,
                     VkDeviceMemory &bufferMemory) {
@@ -2130,6 +2234,8 @@ class HelloTriangleApplication {
     createFramebuffers();
     createCommandPool();
     createTextureImage();
+    createTextureImageView();
+    createTextureSampler();
     createVertexBuffer();
     createIndexBuffer();
     createUniformBuffers();
@@ -2255,6 +2361,10 @@ class HelloTriangleApplication {
     LOG("Cleanup Started");
 
     cleanupSwapChain();
+
+    vkDestroySampler(device, textureSampler, nullptr);
+
+    vkDestroyImageView(device, textureImageView, nullptr);
 
     vkDestroyImage(device, textureImage, nullptr);
     vkFreeMemory(device, textureImageMemory, nullptr);
